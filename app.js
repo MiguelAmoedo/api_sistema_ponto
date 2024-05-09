b1473fbad3d520d2344b88cd467d142ec627568d
MiguelAmoedo <miguelbombs@gmail.com>
2024-05-09 16:45:39 -0300

    hello world13

--- app.js
+++ app.js
@@ -4,27 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 var clienteRouter = require('./routes/cliente');
 var funcionarioRouter = require('./routes/funcionario');
 var chamadoRouter = require('./routes/chamado');
 var pontoRouter = require('./routes/ponto');
 
-
-
-
 require('dotenv').config({path: __dirname + '/.env' })
+
 var app = express();
-
-
 
 // CORS
 app.use(cors());
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// view engine setup'
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -39,8 +30,6 @@ app.use('/funcionario', funcionarioRouter);
 app.use('/chamado', chamadoRouter);
 app.use('/ponto', pontoRouter);
 
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -52,10 +41,12 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the error as JSON
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
 });
-
 
 module.exports = app;
