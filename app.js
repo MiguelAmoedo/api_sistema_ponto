3ddc98c3ef6a5cebc971a0bf1ce3653d5c412c50
MiguelAmoedo <miguelbombs@gmail.com>
2024-05-09 16:09:10 -0300

    hello world6

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ var funcionarioRouter = require('./routes/funcionario');
 var chamadoRouter = require('./routes/chamado');
 var pontoRouter = require('./routes/ponto');
 
+
+
 var app = express();
 
 // CORS
-app.use(cors()); // Habilita o CORS para todas as rotas
+app.use(cors({origin:'http://localhost:3000'}));
+app.options('*', cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,20 +33,12 @@ app.use('/funcionario', funcionarioRouter);
 app.use('/chamado', chamadoRouter);
 app.use('/ponto', pontoRouter);
 
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
